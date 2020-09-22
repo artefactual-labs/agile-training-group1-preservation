02ee19de5d2d4ddf0f01219fb6f930952fc862c7
Justin Simpson <jsimpson@artefactual.com>
2020-09-22 12:38:29 -0700

    updating selector

--- plan/selector.py
+++ plan/selector.py
@@ -1,7 +1,24 @@
 # HI
 
 class Register(): 
-    def getPlanList():
-        return []
+    def __init__ (self, plans):
+        self.plans = plans
+    
+    def DisplayPlanList():
+        # display active plan
+        pass
+    
+    def SelectPlanList(plan):
+        # display active plan
+        pass
+       
+    
+class Plan():
+    def __init__ (self, source, has_normalization_actions):
+        self.source = source
+        self.has_normalization_actions = has_normalization_actions
+    
+
+var myregister = new Register (plans_list)
     
     
